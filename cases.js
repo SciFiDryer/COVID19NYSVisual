cd59f55ae2580abebc51eacf20e81df61d7ec73f
SciFiDryer <54369529+SciFiDryer@users.noreply.github.com>
2020-03-17 22:12:11 -0400

    Add files via upload

---
+++ cases.js
@@ -0,0 +1,61 @@
+var countycases = { "counties" : [
+{"name": "Westchester", "cases": 380}
+,
+{"name": "Nassau", "cases": 131}
+,
+{"name": "Suffolk", "cases": 84}
+,
+{"name": "Albany", "cases": 23}
+,
+{"name": "Rockland", "cases": 22}
+,
+{"name": "Dutchess", "cases": 16}
+,
+{"name": "Orange", "cases": 15}
+,
+{"name": "Monroe", "cases": 10}
+,
+{"name": "Saratoga", "cases": 9}
+,
+{"name": "Ulster", "cases": 8}
+,
+{"name": "Erie", "cases": 7}
+,
+{"name": "Schenectady", "cases": 5}
+,
+{"name": "Allegany", "cases": 2}
+,
+{"name": "Greene", "cases": 2}
+,
+{"name": "Putnam", "cases": 2}
+,
+{"name": "Onondaga", "cases": 2}
+,
+{"name": "Tompkins", "cases": 2}
+,
+{"name": "Broome", "cases": 1}
+,
+{"name": "Clinton", "cases": 1}
+,
+{"name": "Delaware", "cases": 1}
+,
+{"name": "Herkimer", "cases": 1}
+,
+{"name": "Tioga", "cases": 1}
+,
+{"name": "Montgomery", "cases": 1}
+,
+{"name": "Ontario", "cases": 1}
+,
+{"name": "Rensselaer", "cases": 1}
+,
+{"name": "Sullivan", "cases": 1}
+,
+{"name": "Wyoming", "cases": 1}
+,
+{"name": "New York State (Outside of NYC)", "cases": 730}
+,
+{"name": "New York City", "cases": 644}
+,
+{"name": "Total Positive Cases (Statewide)", "cases": 1374}
+]}
